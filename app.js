7ec4ef68190ce2cb72eb60037b4866bea9a1f54d
Yania Gonzalez <yania.catalina@gmail.com>
2025-03-13 19:07:59 -0500

    agrega un amigo

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// Lista de amigos
+let amigos = [];
+
+// agregar un amigo a la lista
+function agregarAmigo() {
+    let inputAmigo = document.getElementById("amigo");
+    let nombre = inputAmigo.value.trim(); // Captura y limpia espacios
+
+    if (nombre === "") {
+        alert("Por favor, inserte un nombre.");
+        return;
+    }
+
+    // Agregar al array
+    amigos.push(nombre);
+
+    // Limpiar el campo de entrada
+    inputAmigo.value = "";
+
+    // Actualizar la lista en pantalla
+    mostrarLista();
+}
