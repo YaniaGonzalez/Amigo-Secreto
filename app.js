fa9feda52929840cfe7ebc2083485f8bb609130c
Yania Gonzalez <yania.catalina@gmail.com>
2025-03-17 20:57:44 -0500

    Agrega funciones para amigos secreto

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 // Lista de amigos
 let amigos = [];
 
-// agregar un amigo a la lista
+// Agrega un amigo a la lista
 function agregarAmigo() {
     let inputAmigo = document.getElementById("amigo");
-    let nombre = inputAmigo.value.trim(); // Captura y limpia espacios
+    let nombre = inputAmigo.value.trim();
 
     if (nombre === "") {
         alert("Por favor, inserte un nombre.");
@@ -14,9 +14,35 @@ function agregarAmigo() {
     // Agregar al array
     amigos.push(nombre);
 
-    // Limpiar el campo de entrada
+    // Limpiar campo
     inputAmigo.value = "";
-
-    // Actualizar la lista en pantalla
     mostrarLista();
+}
+
+// Muestra lista de amigos
+function mostrarLista() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = ""; // Limpiar la lista
+
+    amigos.forEach((amigo) => {
+        let li = document.createElement("li");
+        li.textContent = amigo;
+        lista.appendChild(li);
+    });
+}
+
+// Función para sortear un amigo
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("La lista está vacía. Agregue nombres antes de sortear.");
+        return;
+    }
+	
+
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    let amigoSorteado = amigos[indiceAleatorio];
+
+    // Mostrar resultado
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `<li> El amigo sorteado es: <strong>${amigoSorteado}</strong></li>`;
 }
